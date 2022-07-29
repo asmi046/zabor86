8d32040c7068a30b539b2f1b64451c8e80acba00
asmi046 <asmi046@gmail.com>
2022-07-29 11:57:06 +0300

    404

---
+++ template-parts/uslugu-blk.php
@@ -0,0 +1,27 @@
+<div class="section">
+	<div class="container">
+		<div class="section__title">Наши услуги</div>
+		<div class="services row" id="services">
+		  <div class="services__item">
+			<a href="/zabory" class="s-card" style="background-image: url(/wp-content/themes/zabor/img/zabory.jpg);">
+			  <div class="s-card__name">Заборы <i class="fas fa-chevron-right"></i></div>
+			</a>
+		  </div>
+		  <div class="services__item">
+			<a href="/vorota" class="s-card" style="background-image: url(/wp-content/themes/zabor/img/vorota.jpg);">
+			  <div class="s-card__name">Ворота <i class="fas fa-chevron-right"></i></div>
+			</a>
+		  </div>
+		  <div class="services__item">
+			<a href="/metallokontrukcii" class="s-card" style="background-image: url(/wp-content/themes/zabor/img/metallokontrukcii.jpg);">
+			  <div class="s-card__name">Металлоконтрукции <i class="fas fa-chevron-right"></i></div>
+			</a>
+		  </div>
+		  <div class="services__item">
+			<a href="/krovlya" class="s-card" style="background-image: url(/wp-content/themes/zabor/img/krovlya.jpg);">
+			  <div class="s-card__name">Кровля <i class="fas fa-chevron-right"></i></div>
+			</a>
+		  </div>
+		</div>
+	</div>
+</div>
